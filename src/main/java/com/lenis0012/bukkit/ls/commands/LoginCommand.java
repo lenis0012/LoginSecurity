700d99c4f56a67dc691df8a1576d51b11bf8af62
Charlie Wang <gibsticker@gmail.com>
2013-07-09 18:25:24 -0400

    fixed more command descriptions and removed unused imports

--- src/main/java/com/lenis0012/bukkit/ls/commands/LoginCommand.java
+++ src/main/java/com/lenis0012/bukkit/ls/commands/LoginCommand.java
@@ -5,12 +5,10 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.potion.PotionEffectType;
 
 import com.lenis0012.bukkit.ls.LoginSecurity;
 import com.lenis0012.bukkit.ls.encryption.PasswordManager;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class LoginCommand implements CommandExecutor {
 	@Override
