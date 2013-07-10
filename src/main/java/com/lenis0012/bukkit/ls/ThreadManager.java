839845c525697e6865b4d1f58fb62e12e7e84ec7
Charlie Wang <gibsticker@gmail.com>
2013-07-10 19:05:34 -0400

    fixed players losing location on timeout kick

--- src/main/java/com/lenis0012/bukkit/ls/ThreadManager.java
+++ src/main/java/com/lenis0012/bukkit/ls/ThreadManager.java
@@ -6,124 +6,139 @@ import java.util.Map;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitTask;
 
 public class ThreadManager {
+
 	private LoginSecurity plugin;
 	private int msg = -1, ses = -1, to = -1;
 	private BukkitTask main = null;
 	public Map<String, Integer> session = new HashMap<String, Integer>();
 	public Map<String, Integer> timeout = new HashMap<String, Integer>();
 	private long nextRefresh;
-	
+
 	public ThreadManager(LoginSecurity plugin) {
 		this.plugin = plugin;
 	}
-	
-	public synchronized Map<String, Integer> getSession(){
+
+	public synchronized Map<String, Integer> getSession() {
 		return this.session;
 	}
-	
+
 	public void startMainTask() {
 		this.nextRefresh = System.currentTimeMillis() + 3000000;
 		main = plugin.getServer().getScheduler().runTaskTimer(plugin, new Runnable() {
-
 			@Override
 			public void run() {
 				long time = System.currentTimeMillis();
-				if(time >= nextRefresh) {
-					if(plugin != null) {
-						if(plugin.data != null) {
+				if (time >= nextRefresh) {
+					if (plugin != null) {
+						if (plugin.data != null) {
 							plugin.data.closeConnection();
 							plugin.data.openConnection();
 						}
 					}
-					
+
 					nextRefresh = System.currentTimeMillis() + 3000000;
 				}
 			}
-			
 		}, 20, 20);
 	}
-	
+
 	public void stopMainTask() {
-		if(this.main != null) {
+		if (this.main != null) {
 			this.main.cancel();
 			this.main = null;
 		}
 	}
-	
+
 	public void startMsgTask() {
 		msg = plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
 			public void run() {
-				for(Player player : Bukkit.getServer().getOnlinePlayers()) {
+				for (Player player : Bukkit.getServer().getOnlinePlayers()) {
 					String name = player.getName();
-					if(plugin.authList.containsKey(name)) {
+					if (plugin.authList.containsKey(name)) {
 						boolean register = plugin.authList.get(name);
-						if(register)
-							player.sendMessage(ChatColor.RED+"Please register using /register <password>");
-						else
-							player.sendMessage(ChatColor.RED+"Please login using /login <password>");
+						if (register) {
+							player.sendMessage(ChatColor.RED + "Please register using /register <password>");
+						} else {
+							player.sendMessage(ChatColor.RED + "Please login using /login <password>");
+						}
 					}
 				}
 			}
 		}, 200L, 200L);
 	}
-	
+
 	public void stopMsgTask() {
-		if(msg > 0)
+		if (msg > 0) {
 			plugin.getServer().getScheduler().cancelTask(msg);
+		}
 		msg = -1;
 	}
-	
+
 	public void startSessionTask() {
 		ses = plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
 			public void run() {
 				Iterator<String> it = getSession().keySet().iterator();
-				while(it.hasNext()) {
+				while (it.hasNext()) {
 					String user = it.next();
 					int current = getSession().get(user);
-					if(current >= 1) {
-						current-= 1;
-						getSession().put(user, current);
-					} else
-						it.remove();
-				}
-			}
-		}, 20, 20);
-	}
-	
-	public void stopSessionTask() {
-		if(ses >= 0)
-			plugin.getServer().getScheduler().cancelTask(ses);
-		ses = -1;
-	}
-	
-	public void startTimeoutTask() {
-		ses = plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
-			public void run() {
-				Iterator<String> it = timeout.keySet().iterator();
-				while(it.hasNext()) {
-					String user = it.next();
-					int current = timeout.get(user);
-					if(current >= 1) {
+					if (current >= 1) {
 						current -= 1;
-						timeout.put(user, current);
+						getSession().put(user, current);
 					} else {
 						it.remove();
-						Player player = Bukkit.getPlayer(user);
-						if(player != null && player.isOnline())
-							player.kickPlayer("Login timed out");
 					}
 				}
 			}
 		}, 20, 20);
 	}
-	
+
+	public void stopSessionTask() {
+		if (ses >= 0) {
+			plugin.getServer().getScheduler().cancelTask(ses);
+		}
+		ses = -1;
+	}
+
+	public void startTimeoutTask() {
+		ses = plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
+			@Override
+			public void run() {
+				Iterator<String> it = timeout.keySet().iterator();
+				while (it.hasNext()) {
+					String user = it.next();
+					int current = timeout.get(user);
+					if (current >= 1) {
+						current -= 1;
+						timeout.put(user, current);
+					} else {
+						it.remove();
+						Player player = Bukkit.getPlayer(user);
+						if (player != null && player.isOnline()) {
+							// teleport the player before kicking so that his location is not lost
+							if (plugin.spawntp) {
+								if (plugin.loginLocations.containsKey(user)) {
+									Location fixedLocation = plugin.loginLocations.remove(user);
+									fixedLocation.add(0, 0.2, 0); // fix for players falling into ground
+									player.teleport(fixedLocation);
+								}
+							}
+							player.kickPlayer("Login timed out");
+						}
+					}
+				}
+			}
+		}, 20, 20);
+	}
+
 	public void stopTimeoutTask() {
-		if(to >= 0)
+		if (to >= 0) {
 			plugin.getServer().getScheduler().cancelTask(to);
+		}
 		to = -1;
 	}
 }
