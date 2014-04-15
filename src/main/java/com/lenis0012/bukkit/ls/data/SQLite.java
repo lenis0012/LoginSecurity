06d96feefce348b875d97be9e98061e09a5da8f3
Lennart ten Wolde <0p1q9o2w@hotmail.nl>
2014-04-15 17:59:28 +0200

    fix registering

--- src/main/java/com/lenis0012/bukkit/ls/data/SQLite.java
+++ src/main/java/com/lenis0012/bukkit/ls/data/SQLite.java
@@ -141,7 +141,7 @@ public class SQLite implements DataManager {
 	@Override
 	public void register(String uuid, String password, int encryption, String ip) {
 		try {
-			PreparedStatement ps = con.prepareStatement("INSERT INTO usersunique_user_id,password,encryption,ip) VALUES(?,?,?,?);");
+			PreparedStatement ps = con.prepareStatement("INSERT INTO users(usersunique_user_id,password,encryption,ip) VALUES(?,?,?,?);");
 			ps.setString(1, uuid);
 			ps.setString(2, password);
 			ps.setInt(3, encryption);
