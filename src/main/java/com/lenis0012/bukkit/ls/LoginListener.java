16e6c7f96f3258f13f86c67d82fc8d0196965c2f
mirolm <mirolm@ieti.eu>
2016-01-29 20:58:04 +0200

    Update LoginListener.java

--- src/main/java/com/lenis0012/bukkit/ls/LoginListener.java
+++ src/main/java/com/lenis0012/bukkit/ls/LoginListener.java
@@ -36,7 +36,6 @@ import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.entity.PotionSplashEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-@SuppressWarnings("deprecation")
 public class LoginListener implements Listener {
 
 	private LoginSecurity plugin;
