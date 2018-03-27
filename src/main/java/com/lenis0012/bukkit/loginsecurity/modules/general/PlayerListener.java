832dc6c0f91ba9ddbefde38d5146ad9c5e7f297d
Lennart ten Wolde <lenny.devmail@gmail.com>
2018-03-27 02:05:30 +0200

    Correct padding on command shortcuts. Fixes #81

--- src/main/java/com/lenis0012/bukkit/loginsecurity/modules/general/PlayerListener.java
+++ src/main/java/com/lenis0012/bukkit/loginsecurity/modules/general/PlayerListener.java
@@ -204,9 +204,9 @@ public class PlayerListener implements Listener {
         final LoginSecurityConfig config = LoginSecurity.getConfiguration();
         if(config.isUseCommandShortcut()) {
             if(event.getMessage().startsWith(config.getLoginCommandShortcut() + " ")) {
-                event.setMessage("/login " + event.getMessage().substring(config.getLoginCommandShortcut().length()));
+                event.setMessage("/login " + event.getMessage().substring(config.getLoginCommandShortcut().length() + 1));
             } else if(event.getMessage().startsWith(config.getRegisterCommandShortcut() + " ")) {
-                event.setMessage("/register " + event.getMessage().substring(config.getLoginCommandShortcut().length()));
+                event.setMessage("/register " + event.getMessage().substring(config.getLoginCommandShortcut().length()  + 1));
             }
         }
 
